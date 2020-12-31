3065479113a8346e6b207e69fafa7e02b41e9000
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-31 18:03:05 +0900

    1

--- src/com/javaex/phone/PhoneApp.java
+++ src/com/javaex/phone/PhoneApp.java
@@ -1,11 +1,39 @@
 package com.javaex.phone;
 
+import java.util.Scanner;
+
 public class PhoneApp {
 
 	public static void main(String[] args) {
 		// DB 미니 프로젝트 : 전화번호 관리 프로그램 2
 		
 		// 리스트 등록 수정 삭제 검색 종료
+		boolean run = true;
+		
+		PhoneDao pDao = new PhoneDao();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(run) {
+			pDao.view();
+			int num = sc.nextInt();
+			
+			switch(num) {
+			
+				case 1 :
+					pDao.getList();
+					break;
+				
+				default:
+					break;
+			}
+			
+			run = false;
+		}
+
+		
+		
+		sc.close();
 
 	}
 
--- src/com/javaex/phone/PhoneDao.java
+++ src/com/javaex/phone/PhoneDao.java
@@ -1,6 +1,7 @@
 package com.javaex.phone;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,114 +15,242 @@ public class PhoneDao {
 	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	private String id = "webdb";
 	private String pw = "webdb";
-	
+
 	Connection conn = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
-	int count = 0;
+
+	private int count = 0;
+
 	// 생성자
 	// 메소드 겟셋
 	// 메소드 일반
 
-		
+	// 화면 출력
+	public void view() {
+		System.out.println("******************************************");
+		System.out.println("*          전화번호 관리 프로그램        *");
+		System.out.println("******************************************");
+
+		System.out.println("");
+		System.out.println("1.리스트  2.등록  3.삭제  4.검색  5.종료");
+		System.out.println("------------------------------------------");
+		System.out.print(">메뉴번호: ");
+	}
+
+	// DB접속
 	public void getConnection() {
-		
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
 	}
-	
+
+	// 자원정리
 	public void close() {
-		  try {
-		        if (rs != null) {
-		            rs.close();
-		        }                
-		        if (pstmt != null) {
-		            pstmt.close();
-		        }
-		        if (conn != null) {
-		            conn.close();
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("error:" + e);
-		    }
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
 	}
-	
+
+	//////////////////////////////////////////////////
+
+	// **********리스트**********
 	public List<PhoneVo> getList() {
 		List<PhoneVo> pList = new ArrayList<PhoneVo>();
+
+		getConnection();
+
 		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " SELECT  person_id, ";
+			query += "         name, ";
+			query += "         hp, ";
+			query += "         company ";
+			query += " FROM person ";
 
-		    // 2. Connection 얻어오기
+			pstmt = conn.prepareStatement(query);
 
-		    // 3. SQL문 준비 / 바인딩 / 실행
-		    
-		    // 4.결과처리
+			rs = pstmt.executeQuery();
 
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+			// 4.결과처리
+			while (rs.next()) {
+				int personId = rs.getInt("person_id");
+				String name = rs.getString("name");
+				String hp = rs.getString("hp");
+				String company = rs.getString("company");
+
+				PhoneVo vo = new PhoneVo(personId, name, hp, company);
+				pList.add(vo);
+			}
+
 		} catch (SQLException e) {
-		    System.out.println("error:" + e);
-		} 
-		return pList;
-	}
-	
-	public int phoneInsert() {
-		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-
-		    // 2. Connection 얻어오기
-
-		    // 3. SQL문 준비 / 바인딩 / 실행
-		    
-		    // 4.결과처리
-
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-		    System.out.println("error:" + e);
-		} 
-		return count;
-	}
-	
-	public int phoneUpdate() {
-		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-
-		    // 2. Connection 얻어오기
-
-		    // 3. SQL문 준비 / 바인딩 / 실행
-		    
-		    // 4.결과처리
-
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-		    System.out.println("error:" + e);
-		} 
-		return count;
-	}
-	
-	public int phoneDelete() {
-		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-
-		    // 2. Connection 얻어오기
-
-		    // 3. SQL문 준비 / 바인딩 / 실행
-		    
-		    // 4.결과처리
-
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-		    System.out.println("error:" + e);
+			System.out.println("error:" + e);
 		}
-		return count;
-	}
-	
-	public List<PhoneVo> phoneSearch(){
-		List<PhoneVo> pList = new ArrayList<PhoneVo>();
-		
+
+		close();
+
 		return pList;
 	}
-	
-	
+
+	// **********등록**********
+	public int phoneInsert(PhoneVo pVo) {
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " insert into person ";
+			query += " values(seq_person_id.nextval, ?, ?, ?) ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, pVo.getName());
+			pstmt.setString(2, pVo.getHp());
+			pstmt.setString(3, pVo.getCompany());
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		close();
+
+		return count;
+	}
+
+	// **********수정**********
+	public int phoneUpdate(PhoneVo pVo) {
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " update person ";
+			query += " set name = ?, ";
+			query += "     hp = ?, ";
+			query += "     company = ? ";
+			query += " where person_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, pVo.getName());
+			pstmt.setString(2, pVo.getHp());
+			pstmt.setString(3, pVo.getCompany());
+			pstmt.setInt(4, pVo.getPersonId());
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		close();
+
+		return count;
+	}
+
+	// **********삭제**********
+	public int phoneDelete(int id) {
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " delete from person ";
+			query += " where person_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setInt(1, id);
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		close();
+
+		return count;
+	}
+
+	// **********검색**********
+	public List<PhoneVo> getList(String str) { // 메소드 오버로딩 시키기
+		List<PhoneVo> pList = new ArrayList<PhoneVo>();
+
+		getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " SELECT  person_id, ";
+			query += "         name, ";
+			query += "         hp, ";
+			query += "         company ";
+			query += " FROM person ";
+			query += " where person_id like ? ";
+			query += " or name like ? ";
+			query += " or hp like ? ";
+			query += " or company like ? ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, "%" + str + "%");
+			pstmt.setString(2, "%" + str + "%");
+			pstmt.setString(3, "%" + str + "%");
+			pstmt.setString(4, "%" + str + "%");
+
+			rs = pstmt.executeQuery();
+
+			// 4.결과처리
+			while (rs.next()) {
+				int personId = rs.getInt("person_id");
+				String name = rs.getString("name");
+				String hp = rs.getString("hp");
+				String company = rs.getString("company");
+
+				PhoneVo vo = new PhoneVo(personId, name, hp, company);
+				pList.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		close();
+
+		return pList;
+
+	}
+
 }
